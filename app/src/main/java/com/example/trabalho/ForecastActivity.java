130dc7c9115f718f5750a41b4aa19704b7dbf073
emerson <pduarte.emerson@gmail.com>
2021-06-13 16:49:59 -0300

    wip:layout forecast

--- app/src/main/java/com/example/trabalho/ForecastActivity.java
+++ app/src/main/java/com/example/trabalho/ForecastActivity.java
@@ -2,33 +2,77 @@ package com.example.trabalho;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.trabalho.adapters.MyRecyclerViewAdapter;
 import com.example.trabalho.presenter.contracts.ActivityContract;
 
-public class ForecastActivity extends AppCompatActivity implements ActivityContract.ActivityView {
+import java.util.ArrayList;
+
+public class ForecastActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_forecast);
+//    }
+//
+//    @Override
+//    public Context getContext() {
+//        return this.getApplicationContext();
+//    }
+//
+//    @Override
+//    public void showToast(String message) {
+//        Toast.makeText(this.getApplicationContext(), message, Toast.LENGTH_LONG).show();
+//    }
+//
+//    @Override
+//    public void navigate(Intent intent) {
+//        startActivity(intent);
+//    }
+
+    private MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+
+        // data to populate the RecyclerView with
+        ArrayList<Integer> viewColors = new ArrayList<>();
+        viewColors.add(Color.BLUE);
+        viewColors.add(Color.YELLOW);
+        viewColors.add(Color.MAGENTA);
+        viewColors.add(Color.RED);
+        viewColors.add(Color.BLACK);
+
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        LinearLayoutManager horizontalLayoutManager
+                = new LinearLayoutManager(ForecastActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(horizontalLayoutManager);
+        adapter = new MyRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
-    public Context getContext() {
-        return this.getApplicationContext();
-    }
-
-    @Override
-    public void showToast(String message) {
-        Toast.makeText(this.getApplicationContext(), message, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void navigate(Intent intent) {
-        startActivity(intent);
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on item position " + position, Toast.LENGTH_SHORT).show();
     }
 }
